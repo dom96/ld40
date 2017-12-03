90273993fd8530e2cfa4725a7ef6f564e28544cd
Dominik Picheta <dominikpicheta@gmail.com>
2017-12-03 12:58:33 +0000

    Implement simple camera movement of map.

--- ld40.nim
+++ ld40.nim
@@ -10,6 +10,7 @@ type
   Game = ref object
     window: RenderWindow
     currentMap: Map
+    camera: View
 
   Map = ref object
     texture: Texture
@@ -30,14 +31,23 @@ proc newGame(): Game =
   result = Game(
     window: newRenderWindow(videoMode(screenSize[0], screenSize[1]), "LD40",
                             WindowStyle.Titlebar or WindowStyle.Close),
-    currentMap: newMap(getCurrentDir() / "assets" / "map.png")
+    currentMap: newMap(getCurrentDir() / "assets" / "map.png"),
+    camera: newView()
   )
+
+  result.camera.zoom(0.5)
+  result.window.framerateLimit = 60
 
 proc draw(game: Game) =
   game.window.clear(Black)
   game.currentMap.draw(game.window)
 
+  game.window.view = game.camera
+
   game.window.display()
+
+proc moveCamera(game: Game, dir: Vector2f, mag=16.0) =
+  game.camera.move(dir*mag)
 
 when isMainModule:
   var game = newGame()
@@ -47,6 +57,18 @@ when isMainModule:
       if event.kind == EventType.Closed or
         (event.kind == EventType.KeyPressed and event.key.code == KeyCode.Escape):
           game.window.close()
+
+      if event.kind == EventType.KeyPressed:
+        case event.key.code
+        of KeyCode.Left:
+          game.moveCamera(vec2(-1, 0))
+        of KeyCode.Right:
+          game.moveCamera(vec2(1, 0))
+        of KeyCode.Down:
+          game.moveCamera(vec2(0, 1))
+        of KeyCode.Up:
+          game.moveCamera(vec2(0, -1))
+        else: discard
 
     game.draw()
 
